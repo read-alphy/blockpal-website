29ee88598a539692d0b04fb1b2d5ef0a7c06e75b
chinchin <86435165+chinekop@users.noreply.github.com>
2023-12-08 15:48:15 +0300

    first

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,52 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex  mx-auto items-center min-h-screen flex-col justify-between p-10 ">
+
+      <div className="lg:w-[800px]">
+
+
+        <div className="w-full flex flex-col items-center ">
+
+
+          <Image src="/blockpal.png" width={350} height={350} alt="Blockpal" />
+          <p className="text-zinc-800 text-4xl font-bold">BlockPal - Etherscan AI</p>
+
+
+
+        </div>
+        <div>
+          {/*       <Image src="/etherscan.png" width={150} height={150} alt="Ethereum" /> */}
+
+          <p className="text-zinc-700 text-xl font-normal text-center mt-10">blockpal is an experimental ai assistant for etherscan.
+
+            it is a browser extension. it is not affiliated with etherscan. and it is completley free to use.
+            <br></br> <br></br>
+
+            reach out for bugs: <a target="_blank" href="https://twitter.com/0xTahin" className="text-blue-600 underline font-bold">@0xTahin</a>
+            <br />
+            <br />
+            check our other project <a target="_blank" href="https://alphy.app" className="text-indigo-400 font-bold underline">alphy.app</a> to transcribe and summarize Twitter Spaces and YouTube discussions about crypto</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* 
+      <div
+        className="drop-shadow-lg mt-10 mb-10 "
+      >  <a href="" target="_blank">
+          <button className="flex flex-row normal-case text-blue-950 font-bold py-1 px-6 rounded-xl  items-center border border-zinc-300  ">
+            <Image alt="Chome icon" src="/chrome_icon.png" width={50} height={50} />
+            <span className="text-xl pl-2 r ">Install here</span>
+          </button>
+        </a>
+      </div> */}
+
+      <video className="mt-10" width={900} height={600} autoPlay loop >
+        <source src="/demo.mp4" type="video/mp4" />
+      </video>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
+        {/*   <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -106,7 +113,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </a> */}
       </div>
     </main>
   )
