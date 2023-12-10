4d6e083fa031a01d0d2588fb9d2b4bb8daab34b3
chinchin <86435165+chinekop@users.noreply.github.com>
2023-12-10 12:24:56 +0300

    install on chrome

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,13 @@ export default function Home() {
       <div className="lg:w-[800px]">
         <div className="w-full flex flex-col items-center ">
           <Image src="/blockpal.png" width={250} height={350} alt="Blockpal" />
-          <p className="text-zinc-800 text-4xl font-bold">BlockPal - Etherscan AI</p>
+          <p className="text-zinc-800 text-2xl sm:text-3xl md:text-4xl font-bold text-center">BlockPal - Etherscan AI</p>
+          <a href="https://chromewebstore.google.com/detail/blockpal/bdjjngcmmldpnnlmmecinbokopclkjhd" target="_blank">
+            <button className="flex flex-row items-center text-zinc-800 gap-6 text-xl font-bold border border-zinc-800 drop-shadow-lg rounded-xl px-4 py-2 mt-4 bg-white">
+              <Image src="/chrome_icon.png" width={50} height={50} alt="Etherscan" />
+              <p>Add to your browser</p>
+            </button>
+          </a>
         </div>
         <div>
           <p className="text-zinc-700 text-xl font-normal text-center mt-10">blockpal is an experimental ai assistant for etherscan.
@@ -19,9 +25,10 @@ export default function Home() {
             check our other project <a target="_blank" href="https://alphy.app" className="text-indigo-400 font-bold underline">alphy.app</a> to transcribe and summarize Twitter Spaces and YouTube discussions about crypto</p>
         </div>
       </div>
-      <video className="mt-10" width={900} height={600} autoPlay={true} loop={true} muted={true}>
+      <video className="mt-10 drop-shadow-xl rounded-xl" width={900} height={600} autoPlay={true} loop={true} muted={true}>
         <source src="/demo.mp4" type="video/mp4" />
       </video>
+
 
 
     </main>
